7f7354c5427d7824ec891cbc5403c4da8dbe408c
medziedbelguith <medziedbelguith@gmail.com>
2022-03-29 20:05:17 +0200

    first commit

---
+++ src/main/java/org/sid/SpringJdbcTemplate2PostgreSqlApplication.java
@@ -0,0 +1,30 @@
+package org.sid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class SpringJdbcTemplate2PostgreSqlApplication implements CommandLineRunner{
+	
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcTemplate2PostgreSqlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql="INSERT INTO students (name,email) VALUES ("
+				+"'Nam Ha Minh', 'nam@codejava.net')";
+		int rows=jdbcTemplate.update(sql);
+		if(rows>0)
+		{
+			System.out.println("a new row has been inserted");
+		}
+		
+	}
+
+}
